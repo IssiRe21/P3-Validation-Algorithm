c0c365a1476c468c1ad85e1b93c6ae1de596181b
IssiRe21 <60553960+IssiRe21@users.noreply.github.com>
2020-09-26 14:33:33 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P3___Validation_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] arr = new int[] {1, 1, 1, 3};
+
+
+            static void Comprobar(int[]nums)
+            {
+                bool requisitos = true;
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    if (nums[i] != 1)
+                    {
+                      requisitos = false;                    }
+                    
+                    if (nums[nums.Length-1] != nums.Length-1)
+                    {
+                        requisitos = false;
+                    }
+                
+                }
+
+                if (requisitos == false)
+                {
+                    System.Console.WriteLine("Los datos no cumplen los requisitos");
+                }
+                else
+                { 
+                    System.Console.WriteLine("Los datos cumplen los requisitos");
+                }
+            }
+
+            Comprobar(arr);
+            
+        }
+    }
+}
